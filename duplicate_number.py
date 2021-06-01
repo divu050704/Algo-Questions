eb94ad36cf0ba656e58fbf857fc43dfe62583cd2
Divyanshu Sharma <70474633+divu050704@users.noreply.github.com>
2021-06-01 22:00:21 +0530

    This program will find the dupicate elements in the list with linear search

---
+++ duplicate_number.py
@@ -0,0 +1,17 @@
+#This program will find the duplicate elements in the lsit
+def duplicate(x):#Initiated a function 
+    size  = len(x)#Length of the list
+    for i in range(size):#This will complete the loop till the end of list
+        for j in range(i):#Initiated another loop which will check list except the i 
+            '''
+            if i = 0
+            then it will check 1,2,3,4 
+            '''
+            if x[i] == x[j]:#if it will find the duplicate element in the list then it will print the elemnt
+                print(x[i])
+l = [1,3,4,2,2]
+duplicate(l)
+'''
+OUTPUT OF THE PROGRAM
+2
+'''        
