384c39a5525ea877946bf3faac27430f6fbe510f
Divyanshu Sharma <70474633+divu050704@users.noreply.github.com>
2021-06-15 10:20:34 +0530

    Made the program more responsive

--- duplicate_number.py
+++ duplicate_number.py
@@ -1,5 +1,6 @@
 #This program will find the duplicate elements in the lsit
 def duplicate(x):#Initiated a function 
+    print('Original list:\t',l)
     size  = len(x)#Length of the list
     for i in range(size):#This will complete the loop till the end of list
         for j in range(i):#Initiated another loop which will check list except the i 
@@ -8,7 +9,7 @@ def duplicate(x):#Initiated a function
             then it will check 1,2,3,4 
             '''
             if x[i] == x[j]:#if it will find the duplicate element in the list then it will print the elemnt
-                print(x[i])
+                print('Duplicate element:\t',x[i])
 l = [1,3,4,2,2]
 duplicate(l)
 '''
