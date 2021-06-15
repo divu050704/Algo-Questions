e5684edfd835a80a32e89e5fbb1ac457cb6ccedd
Divyanshu Sharma <70474633+divu050704@users.noreply.github.com>
2021-06-15 11:19:23 +0530

    Made program more responsive

--- merge_2_sorted_arrays_wiyhout_using_extra_space.py
+++ merge_2_sorted_arrays_wiyhout_using_extra_space.py
@@ -1,5 +1,7 @@
 #This program will mrge all the elements of two lists in a singlie sorted list without using extra space
 def merge_list(x,y):#Function for two lists x and y
+    print('First list:\t',x)
+    print('Second list:\t',y)
     main = []#Created an empty list for merging
     size= len(x)#length of first list
     size1 = len(y)#length of second list 
@@ -8,7 +10,7 @@ def merge_list(x,y):#Function for two lists x and y
     for i in range(size1):#Till end of second list 
         main.append(y[i])#Will append all the elements in the list main
     main.sort()# finally sorted the list main
-    print(main)#Printed the merged list 
+    print('Merged list:\t',main)#Printed the merged list 
 n = [1,3,5,7]
 m = [0,2,6,8,9]
 merge_list(n,m)
