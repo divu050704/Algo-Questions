09700b945c14bef045cab6a72356287fd76131b4
Divyanshu Sharma <70474633+divu050704@users.noreply.github.com>
2021-06-15 11:25:34 +0530

    New program added which will minimize the maximum difference between heights(elements)

--- minimize_the_maximum_difference_between_heights.py
+++ minimize_the_maximum_difference_between_heights.py
@@ -1,6 +1,8 @@
 #This program will minimze the maximum difference between heights
 # The value of 'k' is first subtracted from each element one by one and if the value of the number after subtraction comes negative then it will add the value of 'k' instead of subtraction and then will edit the original list
 def minimize_hieghts(x,k):#given the list as 'x' and the value of k as 'k' only
+    print('Original list:\t',x)
+    print('Value of k:\t',k)
     size = len(x)#size of the list
     for i in range(size):#Till end of the list
         number = x[i] - k#First we will try subtraction
@@ -9,11 +11,13 @@ def minimize_hieghts(x,k):#given the list as 'x' and the value of k as 'k' only
         l.pop(i)#First remove the old number 
         l.insert(i,number)#and insert the new number at its place
     l.sort()#First sort the list to get the maximum and minimum number 
-    print(l[int(size-1)]-l[0])#Then finally subtract the larget number from lowest number in the list(last - first)
+    print('Minimum difference between heights:\t',l[int(size-1)]-l[0])#Then finally subtract the larget number from lowest number in the list(last - first)
 l = [1,5,8,10]
 k = 2
 minimize_hieghts(l,k)
 '''
 Output of the program
-5
+Original list:   [1, 5, 8, 10]
+Value of k:      2
+Minimum difference between heights:      5
 '''
